86de6365b9a962d18fc92b41a499bcd84d5241b0
arcaeic <Jory2554@gmail.com>
2018-11-02 00:46:20 -0700

    Added: Locally stored comment blacklisting

--- PIBot/PIBot.py
+++ PIBot/PIBot.py
@@ -1,6 +1,8 @@
 import re
 import praw
 
+#Global Config
+blacklist_file = "comment_blacklist.txt"
 
 def bot_login():
 	reddit = praw.Reddit('PIBot')
@@ -12,8 +14,7 @@ def report(comment):
 	return comment     # Placeholder
 
 
-def scan_comment(comment, domains, email_pattern, phone_pattern, comment_cache):
-	comment_cache.append(comment.id)                      # Add comment to cache, prevents spam
+def scan_comment(comment, domains, email_pattern, phone_pattern):
 	email_regex = re.findall(email_pattern, comment.body)  # Check for email and phone matches
 	phone_regex = re.findall(phone_pattern, comment.body)
 	if email_regex:
@@ -23,29 +24,46 @@ def scan_comment(comment, domains, email_pattern, phone_pattern, comment_cache):
 				report(comment)
 	elif phone_regex:
 		for match in range(0, len(phone_regex)):
-			print_match_text(phone_regex[match], comment.author.name)
+			print_match_text(phone_regex[match], comment)
 			report(comment)
 	return
 
 
-def print_match_text(data, author):
-	print("\n   Found Match!")
-	print("   Phone(s): " + data)
-	print("   Author: " + author)
+def print_match_text(data, comment):
+	print("\nFound Match!")
+	print("Phone(s): " + data)
+	if comment.author:
+		print("Author: " + comment.author.name)
+	return
+
+
+def add_comment_to_blacklist(cid):
+
+	blacklist = open(blacklist_file, "a")
+	blacklist.write(cid + "\n")
+	blacklist.close()
+
+	return
 
 
 def skim(reddit):
-	subreddit = reddit.subreddit('test')  # Placeholder, production will be set to 'all'
+
+	subreddit = reddit.subreddit('Readet')  # Placeholder, production will be set to 'all'
 	email_domains = ['@gmail.com', '@hotmail.com', '@live.ca', '@yahoo.com', '@yahoo.ca', '@aol.com', '@outlook.com']
-	comment_id_cache = []  # Container for previously visited comments
 	email_pattern = r"(\b(\w+(@\w+.[a-z]{0,3})))"
 	phone_pattern = r"(?<!\w)[1 ]?[- ]?(?!800)\(?\d{3}\)?\s?[- ]?\d{3}[- ]?\d{4}(?!\d+?)"
-	for comment in subreddit.stream.comments():  # Look at each new comment as they are submitted
-		if comment.id in comment_id_cache:  # Check if comment has already been visited, not likely
-			print("\n   Found! Oh...I've already replied to this comment, skipping...")
-			continue
-		scan_comment(comment, email_domains, email_pattern, phone_pattern, comment_id_cache)
 
+	comment_blacklist = open(blacklist_file, "r")
+	stripped_blacklist = [x.strip() for x in comment_blacklist.readlines()]
+	comment_blacklist.close()
+
+	for comment in subreddit.stream.comments():  # Look at each new comment as they are submitted
+		if comment.id in stripped_blacklist:  # Check if comment has already been visited, not likely
+			print("\nFound! Oh...I've already found comment " + comment.id + ", skipping...")
+			continue
+		scan_comment(comment, email_domains, email_pattern, phone_pattern)
+		add_comment_to_blacklist(comment.id)
+		stripped_blacklist.append(comment.id)
 
 def main():
 	reddit = bot_login()  # Initiate Reddit instance
